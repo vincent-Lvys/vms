623e605e98678a75b1c2f5c13124a8108bb00ee2
lvxus <lvxus.ying@gmail.com>
2020-01-18 09:38:13 +0800

    修改

--- src/main/java/com/vincent/microservice/controller/UserController.java
+++ src/main/java/com/vincent/microservice/controller/UserController.java
@@ -1,37 +1,78 @@
 package com.vincent.microservice.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.google.gson.Gson;
 import com.vincent.microservice.entity.User;
 import com.vincent.microservice.services.imp.UserService;
+import com.vincent.microservice.util.PageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/user")
 public class UserController {
     @Autowired
     private UserService userService;
     @Autowired
     private HttpServletRequest request;
 
-    @GetMapping("/searchList")
-    public String searchList(){
-        String current = request.getParameter("current");
-        String size = request.getParameter("size");
-        Page<User> userPage = new Page<>(Long.parseLong(current),Long.parseLong(size));
-        Page<User> page = userService.pageList(userPage);
-
-        return new Gson().toJson(page);
+    @GetMapping("/userPage")
+    public ModelAndView userPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/admin-list");
+        return modelAndView;
     }
-    @PostMapping("/saveUser")
-    public String saveUser(@RequestBody User user){
-        boolean save = userService.save(user);
-        if (save)return "success";
-        return "false";
+
+    @GetMapping("/userEditPage/{id}")
+    public ModelAndView userEditPage(@PathVariable(required = false) String id) {
+        ModelAndView modelAndView = new ModelAndView();
+        User user = !"@null@".equals(id) ? userService.getById(id) : new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("/admin-edit");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/searchList", method = RequestMethod.GET)
+    public PageUtil searchList() {
+        String current = request.getParameter("page");
+        String size = request.getParameter("limit");
+        String startDate = request.getParameter("start");
+        String endDate = request.getParameter("end");
+        String keyword = request.getParameter("keyword");
+        Page<User> userPage = new Page<User>(Long.parseLong(current), Long.parseLong(size));
+        userService.pageList(userPage, startDate, endDate, keyword);
+        return new PageUtil(userPage);
+    }
+
+    @PostMapping(value = "/deleteAllUser")
+    public Boolean deleteAllUser() {
+        String selectIds = request.getParameter("selectIds");
+        List<Integer> idList = new ArrayList<>();
+        for (String userId : selectIds.split(",")) {
+            idList.add(Integer.parseInt(userId));
+        }
+        return userService.removeByIds(idList);
+    }
+
+    @PostMapping(value = "/deleteOneUser")
+    public Boolean deleteOneUser() {
+        String id = request.getParameter("id");
+        return userService.removeById(id);
+    }
+
+    @PostMapping("/updateOrSaveUser")
+    public Boolean saveUser(@RequestParam String id, @RequestParam String username, @RequestParam String password) {
+        User user = new User();
+        user.setLoginName(username);
+        user.setPassword(password);
+        if (!StringUtils.isEmpty(id)) user.setId(Integer.parseInt(id));
+        user.setCreationTime(new Date());
+        return userService.saveOrUpdate(user);
     }
 
 }
