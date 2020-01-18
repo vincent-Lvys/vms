f4efac317026ee088a47c36d228d4fcab8943ab7
lvxus <lvxus.ying@gmail.com>
2020-01-18 09:23:52 +0800

    增加前台返回页面结果工具类

---
+++ src/main/java/com/vincent/microservice/util/PageUtil.java
@@ -0,0 +1,29 @@
+package com.vincent.microservice.util;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.Data;
+
+@Data
+public class PageUtil {
+
+    private String code;
+    private Object data;
+    private String message;
+    private long count;
+    private long size;
+
+    public PageUtil(String code, Page data, String message) {
+        this.message = message;
+        this.data = data.getRecords();
+        this.code = code;
+        this.count = data.getTotal();
+        this.size = data.getSize();
+    }
+    public PageUtil(Page data){
+        this.message = "success";
+        this.data = data.getRecords();
+        this.code = "200";
+        this.count = data.getTotal();
+        this.size = data.getSize();
+    }
+}
