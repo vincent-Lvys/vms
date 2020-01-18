c5dc914bdc43ced50dcf2cdddea01fe0770d3957
lvxus <lvxus.ying@gmail.com>
2020-01-18 09:38:58 +0800

    增加用户分页查询

--- src/main/java/com/vincent/microservice/services/IUserService.java
+++ src/main/java/com/vincent/microservice/services/IUserService.java
@@ -2,10 +2,9 @@ package com.vincent.microservice.services;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.github.pagehelper.IPage;
 import com.vincent.microservice.entity.User;
-import org.springframework.stereotype.Service;
 
 public interface IUserService extends IService<User> {
-    Page<User> pageList(Page<User> userPage);
+
+    Page<User> pageList(Page<User> userPage, String startDate, String endDate, String keyword);
 }
--- src/main/java/com/vincent/microservice/services/imp/UserService.java
+++ src/main/java/com/vincent/microservice/services/imp/UserService.java
@@ -1,26 +1,29 @@
 package com.vincent.microservice.services.imp;
 
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.github.pagehelper.IPage;
 import com.vincent.microservice.entity.User;
 import com.vincent.microservice.mapper.UserMapper;
 import com.vincent.microservice.services.IUserService;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @Service
 public class UserService extends ServiceImpl<UserMapper,User> implements IUserService {
     @Resource
     private UserMapper userMapper;
 
+
     @Override
-    public Page<User> pageList(Page<User> userPage) {
-        Page<User> page = userPage.setRecords(userMapper.pageList(userPage));
-        return page;
+    public Page<User> pageList(Page<User> userPage, String startDate, String endDate, String keyword) {
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        if (Strings.isNotBlank(startDate))userQueryWrapper.ge("creation_time",startDate);
+        if (Strings.isNotBlank(endDate))userQueryWrapper.le("creation_time",endDate);
+        if (Strings.isNotBlank(keyword))userQueryWrapper.like("login_name","%"+keyword+"%").or().like("creator","%"+keyword+"%");
+        userMapper.selectPage(userPage,userQueryWrapper.orderByDesc("creation_time"));
+        return userPage;
     }
 }
