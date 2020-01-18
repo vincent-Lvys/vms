eade2e8e6dcc026e1178f598a29b74eaba15703d
lvxus <lvxus.ying@gmail.com>
2020-01-18 09:38:28 +0800

    修改

--- src/main/java/com/vincent/microservice/mapper/UserMapper.java
+++ src/main/java/com/vincent/microservice/mapper/UserMapper.java
@@ -1,12 +1,7 @@
 package com.vincent.microservice.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.vincent.microservice.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-import java.util.List;
 
 public interface UserMapper extends BaseMapper<User> {
-    List<User> pageList(Page<User> userPage);
 }
