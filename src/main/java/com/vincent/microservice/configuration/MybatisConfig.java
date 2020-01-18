284f548d839de0b7fa1a2cf7bfef7518b3fc92af
lvxus <lvxus.ying@gmail.com>
2020-01-18 09:21:35 +0800

    增加config类，修复mybatisplus分页无法实现问题

---
+++ src/main/java/com/vincent/microservice/configuration/MybatisConfig.java
@@ -0,0 +1,16 @@
+package com.vincent.microservice.configuration;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor page = new PaginationInterceptor();
+        page.setDialectType("mysql");
+        return page;
+    }
+}
