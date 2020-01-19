50a029ae1b3add355ac16e5ae760d8fd85780b35
lvxus <lvxus.ying@gmail.com>
2020-01-19 15:16:10 +0800

    角色业务类新增分页查询

--- src/main/java/com/vincent/microservice/services/IRoleService.java
+++ src/main/java/com/vincent/microservice/services/IRoleService.java
@@ -1,7 +1,10 @@
 package com.vincent.microservice.services;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.vincent.microservice.entity.Role;
+import com.vincent.microservice.vo.BaseSearchVo;
 
 public interface IRoleService extends IService<Role> {
+    Page<Role> pageList(Page<Role> page, BaseSearchVo baseSearchVo);
 }
--- src/main/java/com/vincent/microservice/services/imp/RoleService.java
+++ src/main/java/com/vincent/microservice/services/imp/RoleService.java
@@ -1,11 +1,32 @@
 package com.vincent.microservice.services.imp;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.vincent.microservice.entity.Role;
 import com.vincent.microservice.mapper.RoleMapper;
 import com.vincent.microservice.services.IRoleService;
+import com.vincent.microservice.vo.BaseSearchVo;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class RoleService extends ServiceImpl<RoleMapper, Role> implements IRoleService {
+
+    @Resource
+    RoleMapper roleMapper;
+
+    @Override
+    public Page<Role> pageList(Page<Role> page, BaseSearchVo baseSearchVo) {
+        QueryWrapper<Role> wrapper = new QueryWrapper<>();
+        if (Strings.isNotBlank(baseSearchVo.getStart()))wrapper.ge("creation_time",baseSearchVo.getStart());
+        if (Strings.isNotBlank(baseSearchVo.getEnd()))wrapper.le("creation_time",baseSearchVo.getEnd());
+        if (Strings.isNotBlank(baseSearchVo.getKeyword()))wrapper.like("role_name",baseSearchVo.getKeyword())
+                .or().like("description",baseSearchVo.getKeyword())
+                .or().like("creator",baseSearchVo.getKeyword());
+        roleMapper.selectPage(page,wrapper);
+        return page;
+    }
 }
