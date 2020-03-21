399b16b2f0807392a4ccb2dc112227516c07abd0
lvxus <lvxus.ying@gmail.com>
2020-03-21 09:49:29 +0800

    整理

--- vms-activiti/src/main/java/com/vincent/microservice/activiti/controller/ActivitiController.java
+++ vms-activiti/src/main/java/com/vincent/microservice/activiti/controller/ActivitiController.java
@@ -2,9 +2,12 @@ package com.vincent.microservice.activiti.controller;
 
 import com.vincent.microservice.activiti.service.Impl.ActivitiServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.activiti.engine.*;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author gourd
@@ -15,7 +18,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class ActivitiController {
 
     @Autowired
-    private ActivitiServiceImpl activitiService;
+    private RepositoryService repositoryService;
+    @Autowired
+    private RuntimeService runtimeService;
+    @Autowired
+    private TaskService taskService;
+    @Autowired
+    private HistoryService historyService;
+    @Autowired
+    private ProcessEngine processEngine;
+    @Autowired
+    private IdentityService identityService;
+
+    @PostMapping("/workflow/{pdKey}")
+    public String deployWorkFlow(@PathVariable String pdKey){
+        Deployment deploy = repositoryService.createDeployment().addClasspathResource("processes/" + pdKey + ".bpmn").name(pdKey).deploy();
+        return deploy.getId();
+
+    }
+    @PutMapping("/workflow")
+    public String startWorkFlow(){
+        String processKey = "QjFlow";
+        identityService.setAuthenticatedUserId("001");
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(processKey);
+        return processInstance.getId()+"<++++++++>"+processInstance.getProcessDefinitionId();
+    }
+
+
 
 
 }
